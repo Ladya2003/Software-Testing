2e47c8b1e35fa41c9ddfe14b860477d7b32ea01a
Ladya2000 <vlad.vakulenchikk@gmail.com>
2024-04-15 22:32:28 +0300

    add lab9

---
+++ lab9/src/index.js
@@ -0,0 +1,46 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const assert = require('chai').assert;
+
+async function mainPageTitleTest(){
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    try {
+        await driver.get('https://nsv.by/');
+        const title = await driver.findElement(By.css('h1')).getText();
+        assert.include(title, 'Сеть салонов НА СВЯЗИ!', 'Заголовок присутствует на главной странице');
+    } finally {
+        await driver.quit();
+    }
+}
+
+describe('NSV Tests', function(){
+    this.timeout(30000); 
+
+    it('Main page should have the correct title', async () => {
+        await mainPageTitleTest();
+    });
+});
+
+
+
+async function contactPageTest(){
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    try {
+        await driver.get('https://nsv.by/');
+        await driver.findElement(By.linkText('Контакты')).click();
+        await driver.wait(until.elementLocated(By.className('ga_ym_t')), 20000);
+        const phone = await driver.findElement(By.className('ga_ym_t')).getText();
+        assert.include(phone, '+375', 'Номер телефона отображается на странице контактов');
+    } finally {
+        await driver.quit();
+    }
+}
+
+describe('NSV Tests', function(){
+    this.timeout(30000); 
+
+    it('Contact page should display a phone number', async () => {
+        await contactPageTest();
+    });
+});
